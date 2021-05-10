fea5cc72fd1a76e702292a28cd1a76235880db92
Marcel <castrolecram@gmail.com>
2021-05-10 16:46:45 -0400

    Progress made on location card list of map overlay. Scroll not currently working.

--- screens/Map.js
+++ screens/Map.js
@@ -1,28 +1,33 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, FlatList, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import MapView from "react-native-maps";
 import LocationCard from "../components/LocationCard";
 import DisplayButton from "../components/DisplayButton";
 
 // Dummy Data
-const locations = [
+const LOCATIONS = [
     {
+        id: "loc-1",
         name: "West Side Charity",
         needed: ["Clothes", "Toiletries", "Food"],
     },
     {
+        id: "loc-2",
         name: "House of the Charitable Mother, Rosalia",
         needed: ["Clothes", "Toiletries", "Food", "Furniture", "Love"],
     },
     {
+        id: "loc-3",
         name: "Help N' Stuff",
         needed: ["Beds", "Pillows", "Sheets"],
     },
     {
+        id: "loc-4",
         name: "Maybe Helpful, Plenty Loveful",
     },
     {
+        id: "loc-5",
         name: "A Little Bit of Anything for Everybody",
         needed: [
             "Apples",
@@ -35,15 +40,53 @@ const locations = [
             "Dolls",
         ],
     },
+    {
+        id: "loc-6",
+        name: "Juicy Donations",
+        needed: ["Apples", "Oranges", "Tomatoes"],
+    },
+    {
+        id: "loc-7",
+        name: "Fairly Generous House",
+        needed: [
+            "Obtuse",
+            "Rubber Goose",
+            "Green Moose",
+            "Guava Juice",
+            "Giant Snake",
+            "Birthday Cake",
+            "Large Fries",
+            "Chocolate Shake",
+        ],
+    },
+    {
+        id: "loc-8",
+        name: "The Best Charity",
+        needed: ["Papers, Rocks, Scissors"],
+    },
 ];
 
 const Map = () => {
+    const [showList, setShowList] = useState(true);
+
     const centerMap = () => {
         console.log("Map Centered!");
     };
 
     const sortLocations = () => {
         console.log("Locations Sorted!");
+    };
+
+    const handleShowList = () => {
+        setShowList(!showList);
+    };
+
+    const renderItem = ({ item }) => {
+        return (
+            <View style={styles.cardContainer}>
+                <LocationCard name={item.name} tags={item.needed} />
+            </View>
+        );
     };
 
     return (
@@ -60,41 +103,48 @@ const Map = () => {
                     showsUserLocation={true} // TODO
                     rotateEnabled={false}
                     showsCompass={false}
+                    customMapStyle={mapStyle}
                 />
                 <View style={styles.mapOverlay}>
                     <View style={styles.buttonsContainer}>
                         <DisplayButton
-                            text={"Center"}
-                            buttonStyle={[styles.button, { marginRight: 6 }]}
-                            textStyle={styles.buttonText}
-                            onPress={centerMap}
-                        />
-                        <DisplayButton
-                            text={"Sort"}
+                            text={showList ? "Hide List" : "View List"}
                             buttonStyle={styles.button}
                             textStyle={styles.buttonText}
-                            onPress={sortLocations}
+                            onPress={handleShowList}
                         />
-                    </View>
-                    <View style={styles.locationCardsContainer}>
-                        {locations.map((location, i) => (
-                            <View
-                                key={location + i}
-                                style={[
-                                    styles.cardSpacer,
-                                    {
-                                        marginTop: i == 0 ? 6 : 0,
-                                        marginBottom: 6,
-                                    },
+                        <View style={{ flexDirection: "row" }}>
+                            <DisplayButton
+                                text={"Center"}
+                                buttonStyle={[
+                                    styles.button,
+                                    { marginRight: 6 },
                                 ]}
-                            >
-                                <LocationCard
-                                    name={location.name}
-                                    tags={location.needed}
-                                />
-                            </View>
-                        ))}
+                                textStyle={styles.buttonText}
+                                onPress={centerMap}
+                            />
+                            <DisplayButton
+                                text={"Sort"}
+                                buttonStyle={styles.button}
+                                textStyle={styles.buttonText}
+                                onPress={sortLocations}
+                            />
+                        </View>
                     </View>
+                    {showList && (
+                        <View style={styles.listContainer}>
+                            <FlatList
+                                data={LOCATIONS}
+                                renderItem={renderItem}
+                                keyExtractor={(item) => item.id}
+                            />
+                        </View>
+                        // <FlatList
+                        //     data={LOCATIONS}
+                        //     renderItem={renderItem}
+                        //     keyExtractor={(item) => item.id}
+                        // />
+                    )}
                 </View>
             </View>
         </SafeAreaView>
@@ -103,12 +153,13 @@ const Map = () => {
 
 const styles = StyleSheet.create({
     mapOverlay: {
+        flex: 1,
         position: "absolute",
-        left: 6,
-        right: 6,
+        left: 8,
+        right: 8,
     },
     buttonsContainer: {
-        justifyContent: "flex-end",
+        justifyContent: "space-between",
         flexDirection: "row",
         marginTop: 6,
     },
@@ -125,14 +176,50 @@ const styles = StyleSheet.create({
     buttonText: {
         fontSize: 18,
     },
-    locationCardsContainer: {
-        alignItems: "center",
+    listContainer: {
         flex: 1,
+        marginVertical: 4,
     },
-    cardSpacer: {
-        width: "100%",
-        alignItems: "center",
+    cardContainer: {
+        marginVertical: 4,
     },
 });
+
+const mapStyle = [
+    {
+        featureType: "administrative",
+        elementType: "geometry",
+        stylers: [
+            {
+                visibility: "off",
+            },
+        ],
+    },
+    {
+        featureType: "poi",
+        stylers: [
+            {
+                visibility: "off",
+            },
+        ],
+    },
+    {
+        featureType: "road",
+        elementType: "labels.icon",
+        stylers: [
+            {
+                visibility: "off",
+            },
+        ],
+    },
+    {
+        featureType: "transit",
+        stylers: [
+            {
+                visibility: "off",
+            },
+        ],
+    },
+];
 
 export default Map;
